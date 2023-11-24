839f87c49b3573759dc62a00adfd2157f1030241
arslanemrah <148539101+dymetinonal@users.noreply.github.com>
2023-11-24 10:44:06 +0300

    app.js güncellendi alert sağ alta alındı

--- assets/js/app.js
+++ assets/js/app.js
@@ -147,7 +147,7 @@ function showAlert(type, message) {
 
     alert.className = `alert alert-${type}`;
     alert.textContent = message;
-    alert.style = "position: fixed; right: 5px; top: 15px; z-index: 9999; max-width: 387px;"
+    alert.style = "position: fixed; right: 5px; z-index: 9999; max-width: 387px;"
 
     closeButton.className = "close";
     closeButton.type = "button";
